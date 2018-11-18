39c48a0092e31230b11d3dba5f7264cebb3e0d7f
Liz Prescott Desktop <eprescott0216@gmail.com>
2018-11-18 07:56:21 -0500

    Found the correct DateTime pattern

--- truss.rb
+++ truss.rb
@@ -1,12 +1,13 @@
 require "csv"
+require 'pry'
 
+file = CSV.foreach("broken.csv", headers: true) do |row|
+	timestamp = row["Timestamp"]
+	puts timestamp = DateTime.strptime("#{timestamp}", "%D %r")
+	#puts timestamp = DateTime.parse(row["Timestamp"]).iso8601 # Gets the dateTime backwards and only runs twice
 
-CSV.foreach("broken.csv", headers: true) do |row|
-	# puts timestamp = row["Timestamp"]
-	# puts timestamp = DateTime.strptime(timestamp, '%e/%-m/%y %l:%M:%S %p')
-	# puts DateTime.parse("timestamp").iso8601 
-	puts row["Address"].unicode_normalize
-	puts row["ZIP"] 
+	address = row["Address"].unicode_normalize
+	row["ZIP"] 
 	# puts row["FullName"] 
 	# puts row[6] = row[5]-row[4] # dur
 end 
