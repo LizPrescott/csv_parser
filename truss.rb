b9441d99ef06b7a542ad0da3e793dd41b680eca5
Liz Prescott Desktop <eprescott0216@gmail.com>
2018-11-18 08:40:33 -0500

    Now done with DateTime

--- truss.rb
+++ truss.rb
@@ -4,12 +4,15 @@ require 'pry'
 file = CSV.foreach("broken.csv", headers: true) do |row|
 	timestamp = row["Timestamp"]
 	timestamp = DateTime.strptime("#{timestamp}", "%D %r")
-	timestamp = DateTime.parse("#{timestamp}").iso8601 
+	puts timestamp.is_a? DateTime
+	timestamp = DateTime.strptime("#{timestamp}").iso8601
+	timestamp = DateTime.parse("#{timestamp}")
+	puts timestamp.is_a? DateTime
 
 	address = row["Address"].unicode_normalize
 	zip = row["ZIP"] 
 	while zip.length < 5
-		puts zip = zip.prepend("0")
+		zip = zip.prepend("0")
 	end 
 	# puts row["FullName"] 
 	# puts row[6] = row[5]-row[4] # dur
